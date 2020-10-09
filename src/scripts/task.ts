b769df2eb8c0df1d17746c57b452cc77bcc93502
erobert <etiennerobert33@gmail.com>
2020-10-09 21:47:05 +0200

    put component css in js to avoid load problems

--- src/scripts/task.ts
+++ src/scripts/task.ts
@@ -1,6 +1,46 @@
 const template = document.createElement("template");
 template.innerHTML = `
-<link rel="stylesheet" href="./task.css" />
+<style>
+:host {
+  display: block;
+
+  -webkit-tap-highlight-color: transparent;
+  user-select: none;
+  border-radius: 3px;
+  background-color: #ddd;
+  padding: 0.2em 0.4em;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: box-shadow 0.2s ease;
+
+  position: absolute;
+  font-family: "PermanentMarker";
+  font-size: 2em;
+  line-height: 1em; /* ensure that the bounding box is not too high */
+  cursor: pointer;
+  white-space: nowrap;
+}
+
+:host:hover {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.task.completed {
+  opacity: 25%;
+}
+
+:host([status="completed"])::before {
+  content: "";
+  position: absolute;
+  --thickness: 0.2em;
+  height: var(--thickness);
+  top: calc(50% + var(--thickness) / 2);
+  left: 0;
+  right: 0;
+  background-color: black;
+  animation: 1s ease-in-out strikethrough;
+  border-radius: calc(var(--thickness) / 2);
+}
+</style>
 <span id="name"></span>`;
 
 export class Task extends HTMLElement {
